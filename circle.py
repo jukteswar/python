f2a2c67da1362f06f552338b1308881a2afb3c0e
jukteswar Das <jukteswar@gmail.com>
2024-09-27 12:49:43 +0530

    circle

---
+++ circle.py
@@ -0,0 +1,11 @@
+def circle(r):
+    area = 3.14 * r * r
+    cirumference=2*3.24*r
+
+    return(area,cirumference)
+
+a,c=circle(10)
+
+print(f"Area of circle is {a}, circumference of the circle is {c}")
+
+
