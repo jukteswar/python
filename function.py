590e924a5ac6df1a4dbc25ff438b3fa8cc9e8d6b
jukteswar Das <jukteswar@gmail.com>
2024-09-27 12:42:23 +0530

    firstpython

---
+++ function.py
@@ -0,0 +1,22 @@
+'''def hello():
+    print("Hello")
+    print("Jukteswar")
+
+hello()'''
+
+def add(a,b):
+        print(a+b)
+
+
+add(100,20)
+
+def speed(dist,time):
+        print(dist/time)
+
+speed(time=2,dist=10)
+
+def area(radius,pi=3.14):
+        print(pi*radius*radius)
+
+print("The area of", (area(20)))
+        
